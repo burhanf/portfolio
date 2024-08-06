1ba75d15aa759617894ec0393c71949001bb6582
Burhan Faquiri <burhan_faquiri@hotmail.com>
2024-08-06 16:13:45 -0400

    updated experience

--- src/components/Experience/ExperienceItem.jsx
+++ src/components/Experience/ExperienceItem.jsx
@@ -1,10 +1,10 @@
 // eslint-disable-next-line react/prop-types, no-unused-vars
 const ExperienceItem = ({ date, title, company, details, logo }) => {
   return (
-    <ol className="flex flex-col md:flex-row relative border-l border-gradient">
-      <li className="mb-10 ml-4">
+    <ol className="ml-6 md:ml-0 flex flex-col md:flex-row relative border-l border-gradient">
+      <li className="mb-10 ml-4 ">
         <div className="absolute w-3 h-3 bg-gradient rounded-full mt-2.5 -left-1.5 border border-white"></div>
-        <div className="flex flex-col md:flex-row items-start md:items-center gap-4 text-xs md:text-xl">
+        <div className="flex flex-col md:flex-row items-start md:items-center gap-4 text-md md:text-xl">
           <span className="inline-block px-2 py-1 font-semi text-white bg-gradient rounded-md">
             {date}
           </span>
